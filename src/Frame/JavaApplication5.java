73ea7950f4e3026cf2c146cc8147b876c7d6f924
PaulisRa <127845835+PaulisRa@users.noreply.github.com>
2023-07-01 16:03:18 -0500

    ✨ Commit inicial
    Recursos de juego Kids Memory Game

---
+++ src/Frame/JavaApplication5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Frame;
+
+/**
+ *
+ * @author SaHe
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JuegoInterfaz2 P = new JuegoInterfaz2();
+        P.setVisible(true);
+    }
+    
+}
